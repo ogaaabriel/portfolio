46dfe4413cc23278794a08d5b5d0eb55f6fd7947
Gabriel Rodrigues <ogaaabriel@gmail.com>
2023-03-17 14:12:00 -0300

    Added header section.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import './css/style.css'
+
+import Header from "./components/Header";
+
 const App = (): JSX.Element => {
   return (
     <>
-      <h1>Gabriel Rodrigues</h1>
+      <Header />
+      <div>Skills</div>
+      <div>Projects</div>
+      <div>Contact</div>
+      <div>Footer</div>
     </>
   );
 };
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+const Header = (): JSX.Element => {
+  return (
+    <div className="vh-100 d-flex align-items-center justify-content-center text-center bg-gradient bg-dark-blue text-light p-4">
+      <div>
+        <h1 className="display-2">Gabriel Rodrigues</h1>
+        <h2 className="display-4">Desenvolvedor de software</h2>
+        <div className="fs-2">
+          <a
+            className="me-3"
+            href="https://github.com/ogaaabriel"
+            target="_blank"
+          >
+            <i className="bi-github"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/ogaaabriel/" target="_blank">
+            <i className="bi-linkedin"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
